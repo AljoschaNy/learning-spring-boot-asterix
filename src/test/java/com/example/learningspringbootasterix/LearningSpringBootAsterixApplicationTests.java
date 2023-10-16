dcb9b29dc9c18b63004559cf4383cd2e77d91656
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-16 11:01:56 +0200

    Initial commit

---
+++ src/test/java/com/example/learningspringbootasterix/LearningSpringBootAsterixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningspringbootasterix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootAsterixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
